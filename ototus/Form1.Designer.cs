38e15b3bea95443ac738ceb1ea3dc269d3ca3942
Eren İlgün <eren.ilgn.10@hotmail.com>
2024-06-28 00:31:17 +0300

    Klavye girişleri için keyeventler ayarlandı. Klavyeden atamalarda hangi tuşun eklediği gösterildi. Başlatma ve durdurma özellikleri eklendi. Durdurma özelliğinde ms değerinin 100'den az olması durumunda form üzerinde ' - ' tuşu ile durdurulması sağlandı. Tuş seçme şablonu yaratıldı ekstra düzenlemeler yapılacak.

--- ototus/Form1.Designer.cs
+++ ototus/Form1.Designer.cs
@@ -28,21 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelUp = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tus2 = new System.Windows.Forms.Button();
+            this.tus1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelUp
+            // 
+            this.labelUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.labelUp.Location = new System.Drawing.Point(109, 24);
+            this.labelUp.Name = "labelUp";
+            this.labelUp.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelUp.Size = new System.Drawing.Size(207, 58);
+            this.labelUp.TabIndex = 0;
+            this.labelUp.Text = "OTOTUS V1";
+            this.labelUp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(48, 267);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 38);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(264, 267);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(110, 38);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Durdur";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tus2
+            // 
+            this.tus2.Location = new System.Drawing.Point(277, 99);
+            this.tus2.Name = "tus2";
+            this.tus2.Size = new System.Drawing.Size(75, 23);
+            this.tus2.TabIndex = 10;
+            this.tus2.Text = "2. Tuş";
+            this.tus2.UseVisualStyleBackColor = true;
+            // 
+            // tus1
+            // 
+            this.tus1.Location = new System.Drawing.Point(64, 99);
+            this.tus1.Name = "tus1";
+            this.tus1.Size = new System.Drawing.Size(75, 23);
+            this.tus1.TabIndex = 12;
+            this.tus1.Text = "1. Tuş";
+            this.tus1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Tuş Burada gözükecek.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Tuş Burada gözükecek.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 22);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Ms değerini giriniz.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(508, 446);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(429, 365);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tus1);
+            this.Controls.Add(this.tus2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.labelUp);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "OTOTUŞ Version 1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelUp;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button tus2;
+        private System.Windows.Forms.Button tus1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ototus/Form1.cs
+++ ototus/Form1.cs
@@ -1,25 +1,143 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ototus
 {
     public partial class Form1 : Form
     {
+        private Keys selectedKey1 = Keys.None;
+        private Keys selectedKey2 = Keys.None;
+        private int interval = 1000; 
+        private bool isRunning = false;
+        private Thread keyPressThread;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        private const int KEYEVENTF_KEYDOWN = 0x0000; 
+        private const int KEYEVENTF_KEYUP = 0x0002;   
+
         public Form1()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyPress += Form1_KeyPress;
         }
-
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '-')
+            {
+                btnStop_Click(sender, e);
+            }
+        }
+        
         private void Form1_Load(object sender, EventArgs e)
         {
+            tus1.Click += new EventHandler(Tus1_Click);
+            tus2.Click += new EventHandler(Tus2_Click);
+        }
 
+        private void Tus1_Click(object sender, EventArgs e)
+        {
+            selectedKey1 = SelectKey();
+            label1.Text = selectedKey1.ToString();
+        }
+
+        private void Tus2_Click(object sender, EventArgs e)
+        {
+            selectedKey2 = SelectKey();
+            label2.Text = selectedKey2.ToString();
+        }
+
+        private Keys SelectKey()
+        {
+            using (var form = new KeyCaptureForm())
+            {
+                form.ShowDialog();
+                return form.SelectedKey;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (selectedKey1 == Keys.None && selectedKey2 == Keys.None)
+            {
+                MessageBox.Show("Bir tuş seçin");
+                return;
+            }
+
+            if (!int.TryParse(textBox1.Text, out interval) || interval < 1 || interval > 1000)
+            {
+                MessageBox.Show("Lütfen 1 ile 1000 arasında geçerli bir ms değeri girin.");
+                return;
+            }
+            
+            if (isRunning)
+            {
+                MessageBox.Show("Program zaten çalışıyor.");
+                return;
+            }
+
+            isRunning = true;
+            keyPressThread = new Thread(AutoKeyPress);
+            keyPressThread.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+            keyPressThread?.Abort();
+            keyPressThread = null;
+
+            if (keyPressThread == null || !keyPressThread.IsAlive)
+            {
+                keyPressThread = new Thread(AutoKeyPress);
+                keyPressThread.Start();
+            }
+
+
+        }
+        private void AutoKeyPress()
+        {
+            while (isRunning)
+            {
+                if (selectedKey1 != Keys.None)
+                {
+                    keybd_event((byte)selectedKey1, 0, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
+                    Thread.Sleep(50);
+                    keybd_event((byte)selectedKey1, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+                }
+
+                if (selectedKey2 != Keys.None)
+                {
+                    keybd_event((byte)selectedKey2, 0, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
+                    Thread.Sleep(50);
+                    keybd_event((byte)selectedKey2, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+                }
+
+                Thread.Sleep(interval);
+            }
+        }
+    }
+
+    public class KeyCaptureForm : Form
+    {
+        public Keys SelectedKey { get; private set; } = Keys.None;
+
+        public KeyCaptureForm()
+        {
+            this.KeyDown += new KeyEventHandler(KeyCaptureForm_KeyDown);
+            this.Text = "Tuşa Basın";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.AutoSize = true; 
+        }
+
+        private void KeyCaptureForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            SelectedKey = e.KeyCode;
+            this.Close();
         }
     }
 }
